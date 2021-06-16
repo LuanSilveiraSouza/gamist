3b29fbc94f584c82dad801df61ed6e554d183c76
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2021-06-16 10:12:07 -0300

    feat: start app controller

--- src/main/kotlin/com/luanss/gamelist/GameListApplication.kt
+++ src/main/kotlin/com/luanss/gamelist/GameListApplication.kt
@@ -2,6 +2,8 @@ package com.luanss.gamelist
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class GameListApplication
@@ -9,3 +11,13 @@ class GameListApplication
 fun main(args: Array<String>) {
 	runApplication<GameListApplication>(*args)
 }
+
+@RestController
+class AppController {
+	@GetMapping
+	fun health(): Message {
+		return Message("Systems up")
+	}
+}
+
+data class Message(val text: String)
