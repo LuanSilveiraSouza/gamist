a725ff81485fd3ab1469e7bee33bcbff25519285
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2021-07-01 08:43:55 -0300

    feat: add validations to Game

--- src/core/src/main/kotlin/com/luanss/gamist/core/domain/Game.kt
+++ src/core/src/main/kotlin/com/luanss/gamist/core/domain/Game.kt
@@ -1,17 +1,28 @@
 package com.luanss.gamist.core.domain
 
+import com.luanss.gamist.core.exception.InvalidReleaseYearException
 import com.luanss.gamist.core.exception.InvalidScoreException
+import java.text.SimpleDateFormat
+import java.util.*
 
-data class Game(
+class Game(
+        private var id: UUID,
         private var name: String,
         private var releaseYear: Int,
-        private var developer: String,
-        private val score: Int
+        private var score: Int,
+        private var playTime: Float,
+        private var description: String,
 ) {
+    constructor(
+            name: String,
+            releaseYear: Int,
+            score: Int,
+            playTime: Float,
+            description: String,
+    ) : this(UUID.fromString(""), name, releaseYear, score, playTime, description) {}
+
     init {
-        if (score < 0 || score > 100) {
-            throw InvalidScoreException()
-        }
+        validate()
     }
 
     fun setName(name: String) {
@@ -20,14 +31,6 @@ data class Game(
 
     fun getName(): String {
         return this.name
-    }
-
-    fun setDeveloper(developer: String) {
-        this.developer = developer
-    }
-
-    fun getDeveloper(): String {
-        return this.developer
     }
 
     fun setReleaseYear(releaseYear: Int) {
@@ -40,5 +43,52 @@ data class Game(
 
     fun getScore(): Int {
         return this.score
+    }
+
+    fun setScore(score: Int) {
+        this.score = score
+    }
+
+    fun setPlayTime(playTime: Float) {
+        this.playTime = playTime
+    }
+
+    fun getPlayTime(): Float {
+        return this.playTime
+    }
+
+    fun setDescription(description: String) {
+        this.description = description
+    }
+
+    fun getDescription(): String {
+        return this.description
+    }
+
+    fun validate() {
+        checkScore()
+        checkPlayTime()
+        checkReleaseYear()
+    }
+
+    private fun checkScore() {
+        if (this.score < 0 || this.score > 100) {
+            throw InvalidScoreException()
+        }
+    }
+
+    private fun checkReleaseYear() {
+        if (this.releaseYear < 1970) {
+            throw InvalidReleaseYearException()
+        }
+    }
+
+    private fun checkPlayTime() {
+        val currentDate = Date()
+        val formatter = SimpleDateFormat("yyyy")
+
+        if (this.playTime > 0 && this.releaseYear > Integer.parseInt(formatter.format(currentDate))) {
+            throw InvalidReleaseYearException()
+        }
     }
 }
